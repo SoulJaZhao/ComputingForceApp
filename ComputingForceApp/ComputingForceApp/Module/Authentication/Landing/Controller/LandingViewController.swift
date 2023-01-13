6e8225b0645052e442b6fe0ed9fb36bd8832ef3e
longzhao-gd <long.zhao@greendotcorp.com>
2023-01-13 09:36:34 +0800

    设置Navigation

--- ComputingForceApp/ComputingForceApp/Module/Authentication/Landing/Controller/LandingViewController.swift
+++ ComputingForceApp/ComputingForceApp/Module/Authentication/Landing/Controller/LandingViewController.swift
@@ -19,6 +19,16 @@ class LandingViewController: BaseViewController<LandingViewModel> {
         setupUI()
     }
     
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        navigationController?.setNavigationBarHidden(true, animated: false)
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        navigationController?.setNavigationBarHidden(false, animated: false)
+    }
+    
     override func bind(viewModel: LandingViewModel, storeBindingsIn cancellables: inout Set<AnyCancellable>) {
         super.bind(viewModel: viewModel, storeBindingsIn: &subscriptions)
         
